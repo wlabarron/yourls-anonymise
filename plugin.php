6f81693699194d4250ee828058fd1d1e849221ae
Andrew Barron <hello@awmb.uk>
2020-01-15 16:42:31 +0000

    Minor addition to description, adding author URI

--- plugin.php
+++ plugin.php
@@ -2,10 +2,10 @@
 /*
   Plugin Name: Anonymise
   Plugin URI: https://github.com/wlabarron/yourls-anonymise
-  Description: Anonymise the IP address and user agent in YOURLS logs.
+  Description: Anonymise the IP address and user agent in YOURLS logs. User's country of origin is now also not recorded, since it's based on IP.
   Version: 1.0
   Author: wlabarron
-  Author URI:
+  Author URI: https://github.com/wlabarron/
 */
 
 yourls_add_filter( 'get_IP', 'wlabarron_anonymise_IP' );
