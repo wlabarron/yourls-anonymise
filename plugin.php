24a748fcc966656a49a3409669bbbaf48d38c728
Andrew Barron <hello@awmb.uk>
2020-01-15 16:36:31 +0000

    Now anonymises IP address and user agent

--- plugin.php
+++ plugin.php
@@ -1,34 +1,22 @@
 <?php
 /*
-Plugin Name: Pseudonymize Plugin
-Plugin URI: http://github.com/ubicoo/yourls-pseudonymize
-Description: Pseudonymize IP addresses (remove last segment). Supports IPv4 and IPv6.
-Version: 1.1
-Author: Ubicoo
-Author URI: http://www.ubicoo.com
- */
+  Plugin Name: Anonymise
+  Plugin URI: https://github.com/wlabarron/yourls-anonymise
+  Description: Anonymise the IP address and user agent in YOURLS logs.
+  Version: 1.0
+  Author: wlabarron
+  Author URI:
+*/
 
+yourls_add_filter( 'get_IP', 'wlabarron_anonymise_IP' );
+yourls_add_filter( 'get_user_agent', 'wlabarron_anonymise_user_agent' );
 
-yourls_add_filter( 'get_IP', 'ubicoo_pseudonymize_IP' );
-
-function ubicoo_pseudonymize_IP( $ip ) {
-    if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-        $segments = explode(":", $ip);
-        $segments[count($segments)-1] = 0;
-
-        $pseudo_IP = implode(":", $segments);
-
-        # FIXME: also handle IPv4 addresses at the end of IPv6, like ::ffff:127.0.0.1
-        
-    } elseif(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-        $segments = explode(".", $ip);
-        $segments[3] = 0;
-
-        $pseudo_IP = implode(".", $segments);
-    } else {
-        $pseudo_IP = $ip;
-    }
-
-    return $pseudo_IP;
+function wlabarron_anonymise_IP( $ip ) {
+    return "-";
 }
 
+function wlabarron_anonymise_user_agent( $ua ) {
+    return "-";
+}
+
+?>
